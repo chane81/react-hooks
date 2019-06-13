f74374fff741c8f5b38b6aa943eee9008b29881d
BM <chane81@naver.com>
2019-06-13 13:51:28 +0900

    lodash/fp 로 변경

--- src/context/MyContext.js
+++ src/context/MyContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useReducer } from 'react';
-import _ from 'lodash';
+import _ from 'lodash/fp';
 
 const initState = {
 	counter: 1,
@@ -7,29 +7,16 @@ const initState = {
 };
 const MyContext = createContext(initState);
 
-/**
- *  로다쉬 merge 를 이용한 불변성을 유지한 객체 리턴 state
- */
 const reducer = (state, action) => {
 	switch (action.name) {
 		case 'INCREMENT': {
-			// return {
-			// 	...state,
-			// 	counter: state.counter + 1
-			// };
-
-			return _.merge({}, state, { counter: state.counter + 1 });
+			return _.merge(state, { counter: state.counter + 1 });
 		}
 		case 'DECREMENT': {
-			// return {
-			// 	...state,
-			// 	counter: state.counter - 1
-			// };
-
-			return _.merge({}, state, { counter: state.counter - 1 });
+			return _.merge(state, { counter: state.counter - 1 });
 		}
 		case 'SET_NICKNAME': {
-			return _.merge({}, state, { nickname: action.value });
+			return _.set('nickname', action.value, state);
 		}
 		default:
 			return state;
